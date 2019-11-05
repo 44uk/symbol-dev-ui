761339b524b17b447d343aba127ffcbedc21cc70
Yoshiyuki Ieyama <yukku0423@gmail.com>
2019-11-05 12:47:56 +0900

    add mini.css

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ import {
   Link
 } from "react-router-dom";
 
-import logo from './logo.svg';
+import 'mini.css'
 import './App.css';
+
+import logo from './logo.svg';
 
 import Example from './pages/Example';
 
