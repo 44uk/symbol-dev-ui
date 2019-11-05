b10fd271f1e42fd822130056fb437fd7b746d5b0
Yoshiyuki Ieyama <yukku0423@gmail.com>
2019-11-05 13:28:18 +0900

    add _overwrite.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import 'mini.css';
+import './styles/_overwrite.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
