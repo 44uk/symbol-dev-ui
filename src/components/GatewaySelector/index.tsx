5b186d542f46e1431ada3680c2849124a5f16b07
Yoshiyuki Ieyama <yukku0423@gmail.com>
2019-11-10 12:49:31 +0900

    GatewaySelector

--- src/components/GatewaySelector/index.tsx
+++ src/components/GatewaySelector/index.tsx
@@ -1,28 +1,32 @@
+import GATEWAY_LIST from 'resources/gateway.json'
 import React, { useEffect, useState } from 'react';
 import createPersistedState from 'use-persisted-state'
-import GATEWAY_LIST from 'resources/gateway.json'
 
-const useCurrentGatewayState = createPersistedState('current-node')
+const useCurrentGatewayState = createPersistedState('current-gateway')
 const useGatewayListState = createPersistedState('gateway-list')
 
 interface IProps {
   onAvailable?: () => void
 }
 
+interface INodeInfo {
+
+}
+
 export const NodeSelector: React.FC = (
   onAvailable
 ) => {
-  const [gwList] = useGatewayListState(GATEWAY_LIST)
   const [gw, setGw] = useCurrentGatewayState(GATEWAY_LIST[0])
+  const [gwList] = useGatewayListState(GATEWAY_LIST)
   const [availability, setAvailability] = useState(false)
 
   useEffect(() => {
-    const url = `${gw}/chain/height`
-    console.debug(url)
+    const url = `${gw}/node/info`
     setAvailability(false)
     fetch(url)
+      .then(resp => resp.json())
       .then(resp => {
-        console.debug(resp)
+        // console.debug(resp.networkIdentifier)
         setAvailability(true)
       })
       .catch(error => {
