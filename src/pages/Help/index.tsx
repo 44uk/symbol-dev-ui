1dd22892f20f6cdfee7444d753af09c9078545bb
Yoshiyuki Ieyama <yukku0423@gmail.com>
2019-11-10 12:48:08 +0900

    update help page

--- src/pages/Help/index.tsx
+++ src/pages/Help/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import ImageArmyKnife from './army_knife.png'
+
 export const Help: React.FC = () => {
   return (
-    <div>
-      <p>Help Page.</p>
-    </div>
+<div className="card fluid">
+  <div className="section">
+    <h1 className="double-padded">
+      nem2(catapult) development Utility Interface
+      <small>This is a tools for nem2.</small>
+    </h1>
+    <p><a href="https://github.com/44uk/nem2-dev-ui" target="_blank" rel="noopener noreferrer">GitHub: 44uk/nem2-dev-ui</a></p>
+
+    <figure>
+      <img src={ImageArmyKnife} alt="ArmyKnife" width="120" />
+      <figcaption><a target="_blank" rel="noopener noreferrer" href="https://www.irasutoya.com/2017/05/blog-post_395.html">十徳ナイフのイラスト | かわいいフリー素材集 いらすとや</a></figcaption>
+    </figure>
+  </div>
+</div>
   );
 }
 
